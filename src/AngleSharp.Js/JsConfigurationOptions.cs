0df08f9b1183723db471057e0a5956f04a0f3101
vladomelchenko <vladomelchenko98@gmail.com>
2020-07-15 18:11:29 +0300

    Create JsConfigurationOptions.cs

---
+++ src/AngleSharp.Js/JsConfigurationOptions.cs
@@ -0,0 +1,13 @@
+namespace AngleSharp.Js
+{
+    /// <summary>
+    /// Configuration options for js configuration
+    /// </summary>
+    public class JsConfigurationOptions
+    {
+        /// <summary>
+        /// if false, script execution on the page will be turned off 
+        /// </summary>
+        public bool UseDefaultNavigationHandler { get; set; }
+    }
+}
